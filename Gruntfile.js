ae9d3181547b0aa6bad96aff43b9f6503ad87dca
Fengyuan Chen <cfy.net@gmail.com>
2014-08-09 17:08:30 +0800

    Update

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,13 +12,16 @@ module.exports = function(grunt) {
                 " * Released under the <%= pkg.license.type %> license\n" +
                 " */\n",
         clean: {
-            files: ["build/<%= pkg.version %>", "dist/"]
+            dist: ["dist/"],
+            build: ["build/<%= pkg.version %>.<%= grunt.template.today('yyyymmdd') %>"],
+            release: ["release/<%= pkg.version %>"],
+            docs: ["../fengyuanchen.github.io/<%= pkg.name %>/"]
         },
         jshint: {
             options: {
                 jshintrc: ".jshintrc"
             },
-            files: ["*.js", "src/<%= pkg.name %>.js"]
+            files: ["Gruntfile.js", "src/<%= pkg.name %>.js"]
         },
         uglify: {
             dist: {
@@ -47,7 +50,28 @@ module.exports = function(grunt) {
                 expand: true,
                 cwd: "dist/",
                 src: "*.js",
-                dest: "build/<%= pkg.version %>/",
+                dest: "build/<%= pkg.version %>.<%= grunt.template.today('yyyymmdd') %>/",
+                filter: "isFile"
+            },
+            release: {
+                expand: true,
+                cwd: "dist/",
+                src: "*.js",
+                dest: "release/<%= pkg.version %>/",
+                filter: "isFile"
+            },
+            sync: {
+                expand: true,
+                cwd: "dist/",
+                src: "**",
+                dest: "../fengyuanchen.github.io/dist/",
+                filter: "isFile"
+            },
+            docs: {
+                expand: true,
+                cwd: "docs/",
+                src: "**",
+                dest: "../fengyuanchen.github.io/<%= pkg.name %>/",
                 filter: "isFile"
             }
         },
@@ -57,7 +81,10 @@ module.exports = function(grunt) {
         }
     });
 
+    // Loading dependencies
     require("load-grunt-tasks")(grunt);
 
-    grunt.registerTask("default", ["clean", "jshint", "uglify", "copy:dist", "usebanner", "copy:build"]);
+    grunt.registerTask("release", ["clean:release", "copy:release"]);
+    grunt.registerTask("docs", ["clean:docs", "copy:sync", "copy:docs"]);
+    grunt.registerTask("default", ["clean:dist", "clean:build", "jshint", "uglify", "copy:dist", "usebanner", "copy:build"]);
 };
