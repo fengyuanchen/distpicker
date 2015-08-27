a01704f9e9079332843b66d758262ec9c544dbc2
Fengyuan Chen <cfy.org@gmail.com>
2015-08-27 22:13:50 +0800

    Added docs

---
+++ docs/js/main.js
@@ -0,0 +1,47 @@
+$(function () {
+
+  'use strict';
+
+  var $distpicker = $('#distpicker');
+
+  $distpicker.distpicker({
+    province: '福建省',
+    city: '厦门市',
+    district: '思明区'
+  });
+
+  $('#reset').click(function () {
+    $distpicker.distpicker('reset');
+  });
+
+  $('#reset-deep').click(function () {
+    $distpicker.distpicker('reset', true);
+  });
+
+  $('#destroy').click(function () {
+    $distpicker.distpicker('destroy');
+  });
+
+  $('#distpicker1').distpicker();
+
+  $('#distpicker2').distpicker({
+    province: '---- 所在省 ----',
+    city: '---- 所在市 ----',
+    district: '---- 所在区 ----'
+  });
+
+  $('#distpicker3').distpicker({
+    province: '浙江省',
+    city: '杭州市',
+    district: '西湖区'
+  });
+
+  $('#distpicker4').distpicker({
+    placeholder: false
+  });
+
+  $('#distpicker5').distpicker({
+    autoSelect: false
+  });
+
+});
