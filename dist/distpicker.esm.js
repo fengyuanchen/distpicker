520b4951a51b67198318493199513322516a600d
luwenjiechn <luwenjiechn@qq.com>
2018-03-17 14:20:27 +0800

    兼容ie8

--- dist/distpicker.esm.js
+++ dist/distpicker.esm.js
@@ -5,7 +5,7 @@
  * Copyright (c) 2014-2018 Chen Fengyuan
  * Released under the MIT license
  *
- * Date: 2018-03-01T12:52:03.658Z
+ * Date: 2018-03-17T06:12:52.316Z
  */
 
 import $ from 'jquery';
@@ -4143,8 +4143,6 @@ var WINDOW = typeof window !== 'undefined' ? window : {};
 var NAMESPACE = 'distpicker';
 var EVENT_CHANGE = 'change';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var DEFAULT_CODE = 100000;
@@ -4163,203 +4161,192 @@ var Distpicker = function () {
     this.init();
   }
 
-  _createClass(Distpicker, [{
-    key: 'init',
-    value: function init() {
-      var _this = this;
+  Distpicker.prototype.init = function init() {
+    var _this = this;
 
-      var options = this.options;
+    var options = this.options;
 
-      var $selects = this.$element.find('select');
-      var length = $selects.length;
+    var $selects = this.$element.find('select');
+    var length = $selects.length;
 
-      var data = {};
+    var data = {};
 
-      $selects.each(function (i, select) {
-        return $.extend(data, $(select).data());
-      });
+    $selects.each(function (i, select) {
+      return $.extend(data, $(select).data());
+    });
 
-      $.each([PROVINCE, CITY, DISTRICT], function (i, type) {
-        if (data[type]) {
-          options[type] = data[type];
-          _this['$' + type] = $selects.filter('[data-' + type + ']');
-        } else {
-          _this['$' + type] = length > i ? $selects.eq(i) : null;
-        }
-      });
-
-      this.bind();
-
-      // Reset all the selects (after event binding)
-      this.reset();
-      this.ready = true;
-    }
-  }, {
-    key: 'bind',
-    value: function bind() {
-      var _this2 = this;
-
-      if (this.$province) {
-        this.$province.on(EVENT_CHANGE, this.onChangeProvince = $.proxy(function () {
-          _this2.output(CITY);
-          _this2.output(DISTRICT);
-        }, this));
-      }
-
-      if (this.$city) {
-        this.$city.on(EVENT_CHANGE, this.onChangeCity = $.proxy(function () {
-          return _this2.output(DISTRICT);
-        }, this));
-      }
-    }
-  }, {
-    key: 'unbind',
-    value: function unbind() {
-      if (this.$province) {
-        this.$province.off(EVENT_CHANGE, this.onChangeProvince);
-      }
-
-      if (this.$city) {
-        this.$city.off(EVENT_CHANGE, this.onChangeCity);
-      }
-    }
-  }, {
-    key: 'output',
-    value: function output(type) {
-      var options = this.options,
-          placeholders = this.placeholders;
-
-      var $select = this['$' + type];
-
-      if (!$select || !$select.length) {
-        return;
-      }
-
-      var code = void 0;
-
-      switch (type) {
-        case PROVINCE:
-          code = DEFAULT_CODE;
-          break;
-
-        case CITY:
-          code = this.$province && (this.$province.find(':selected').data('code') || '');
-          break;
-
-        case DISTRICT:
-          code = this.$city && (this.$city.find(':selected').data('code') || '');
-          break;
-      }
-
-      var districts = this.getDistricts(code);
-      var value = options[type];
-      var data = [];
-      var matched = false;
-
-      if ($.isPlainObject(districts)) {
-        $.each(districts, function (i, name) {
-          var selected = name === value;
-
-          if (options.valueType === 'code') {
-            selected = i === String(value);
-          }
-
-          if (selected) {
-            matched = true;
-          }
-
-          data.push({
-            code: i,
-            name: name,
-            value: options.valueType === 'name' ? name : i,
-            selected: selected
-          });
-        });
-      }
-
-      if (!matched) {
-        var autoselect = options.autoselect || options.autoSelect;
-
-        if (data.length && (type === PROVINCE && autoselect > 0 || type === CITY && autoselect > 1 || type === DISTRICT && autoselect > 2)) {
-          data[0].selected = true;
-        }
-
-        // Save the unmatched value as a placeholder at the first output
-        if (!this.ready && value) {
-          placeholders[type] = value;
-        }
-      }
-
-      // Add placeholder option
-      if (options.placeholder) {
-        data.unshift({
-          code: '',
-          name: placeholders[type],
-          value: '',
-          selected: false
-        });
-      }
-
-      if (data.length) {
-        $select.html(this.getList(data));
+    $.each([PROVINCE, CITY, DISTRICT], function (i, type) {
+      if (data[type]) {
+        options[type] = data[type];
+        _this['$' + type] = $selects.filter('[data-' + type + ']');
       } else {
-        $select.empty();
+        _this['$' + type] = length > i ? $selects.eq(i) : null;
       }
+    });
 
-      $select.trigger(EVENT_CHANGE);
+    this.bind();
+
+    // Reset all the selects (after event binding)
+    this.reset();
+    this.ready = true;
+  };
+
+  Distpicker.prototype.bind = function bind() {
+    var _this2 = this;
+
+    if (this.$province) {
+      this.$province.on(EVENT_CHANGE, this.onChangeProvince = $.proxy(function () {
+        _this2.output(CITY);
+        _this2.output(DISTRICT);
+      }, this));
     }
 
-    // eslint-disable-next-line class-methods-use-this
+    if (this.$city) {
+      this.$city.on(EVENT_CHANGE, this.onChangeCity = $.proxy(function () {
+        return _this2.output(DISTRICT);
+      }, this));
+    }
+  };
 
-  }, {
-    key: 'getList',
-    value: function getList(data) {
-      var list = [];
+  Distpicker.prototype.unbind = function unbind() {
+    if (this.$province) {
+      this.$province.off(EVENT_CHANGE, this.onChangeProvince);
+    }
 
-      $.each(data, function (i, n) {
-        var attrs = ['data-code="' + n.code + '"', 'data-text="' + n.name + '"', 'value="' + n.value + '"'];
+    if (this.$city) {
+      this.$city.off(EVENT_CHANGE, this.onChangeCity);
+    }
+  };
 
-        if (n.selected) {
-          attrs.push('selected');
+  Distpicker.prototype.output = function output(type) {
+    var options = this.options,
+        placeholders = this.placeholders;
+
+    var $select = this['$' + type];
+
+    if (!$select || !$select.length) {
+      return;
+    }
+
+    var code = void 0;
+
+    switch (type) {
+      case PROVINCE:
+        code = DEFAULT_CODE;
+        break;
+
+      case CITY:
+        code = this.$province && (this.$province.find(':selected').data('code') || '');
+        break;
+
+      case DISTRICT:
+        code = this.$city && (this.$city.find(':selected').data('code') || '');
+        break;
+    }
+
+    var districts = this.getDistricts(code);
+    var value = options[type];
+    var data = [];
+    var matched = false;
+
+    if ($.isPlainObject(districts)) {
+      $.each(districts, function (i, name) {
+        var selected = name === value;
+
+        if (options.valueType === 'code') {
+          selected = i === String(value);
         }
 
-        list.push('<option ' + attrs.join(' ') + '>' + n.name + '</option>');
+        if (selected) {
+          matched = true;
+        }
+
+        data.push({
+          code: i,
+          name: name,
+          value: options.valueType === 'name' ? name : i,
+          selected: selected
+        });
       });
-
-      return list.join('');
     }
 
-    // eslint-disable-next-line class-methods-use-this
+    if (!matched) {
+      var autoselect = options.autoselect || options.autoSelect;
 
-  }, {
-    key: 'getDistricts',
-    value: function getDistricts() {
-      var code = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : DEFAULT_CODE;
+      if (data.length && (type === PROVINCE && autoselect > 0 || type === CITY && autoselect > 1 || type === DISTRICT && autoselect > 2)) {
+        data[0].selected = true;
+      }
 
-      return DISTRICTS[code] || null;
-    }
-  }, {
-    key: 'reset',
-    value: function reset(deep) {
-      if (!deep) {
-        this.output(PROVINCE);
-        this.output(CITY);
-        this.output(DISTRICT);
-      } else if (this.$province) {
-        this.$province.find(':first').prop('selected', true).end().trigger(EVENT_CHANGE);
+      // Save the unmatched value as a placeholder at the first output
+      if (!this.ready && value) {
+        placeholders[type] = value;
       }
     }
-  }, {
-    key: 'destroy',
-    value: function destroy() {
-      this.unbind();
+
+    // Add placeholder option
+    if (options.placeholder) {
+      data.unshift({
+        code: '',
+        name: placeholders[type],
+        value: '',
+        selected: false
+      });
     }
-  }], [{
-    key: 'setDefaults',
-    value: function setDefaults(options) {
-      $.extend(DEFAULTS, $.isPlainObject(options) && options);
+
+    if (data.length) {
+      $select.html(this.getList(data));
+    } else {
+      $select.empty();
     }
-  }]);
+
+    $select.trigger(EVENT_CHANGE);
+  };
+
+  // eslint-disable-next-line class-methods-use-this
+
+
+  Distpicker.prototype.getList = function getList(data) {
+    var list = [];
+
+    $.each(data, function (i, n) {
+      var attrs = ['data-code="' + n.code + '"', 'data-text="' + n.name + '"', 'value="' + n.value + '"'];
+
+      if (n.selected) {
+        attrs.push('selected');
+      }
+
+      list.push('<option ' + attrs.join(' ') + '>' + n.name + '</option>');
+    });
+
+    return list.join('');
+  };
+
+  // eslint-disable-next-line class-methods-use-this
+
+
+  Distpicker.prototype.getDistricts = function getDistricts() {
+    var code = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : DEFAULT_CODE;
+
+    return DISTRICTS[code] || null;
+  };
+
+  Distpicker.prototype.reset = function reset(deep) {
+    if (!deep) {
+      this.output(PROVINCE);
+      this.output(CITY);
+      this.output(DISTRICT);
+    } else if (this.$province) {
+      this.$province.find(':first').prop('selected', true).end().trigger(EVENT_CHANGE);
+    }
+  };
+
+  Distpicker.prototype.destroy = function destroy() {
+    this.unbind();
+  };
+
+  Distpicker.setDefaults = function setDefaults(options) {
+    $.extend(DEFAULTS, $.isPlainObject(options) && options);
+  };
 
   return Distpicker;
 }();
